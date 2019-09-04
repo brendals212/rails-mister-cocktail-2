79878e60eaff93c3c6c406434d4e207d06cfb1aa
Brenda Sanchez <brendals212@gmail.com>
2019-09-04 17:06:49 +0300

    Controllers Views and Models Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'doses/index'
+  get 'doses/create'
+  get 'doses/new'
+  get 'doses/edit'
+  get 'doses/show'
+  get 'doses/update'
+  get 'doses/destroy'
+  get 'cocktails/index'
+  get 'cocktails/create'
+  get 'cocktails/new'
+  get 'cocktails/edit'
+  get 'cocktails/show'
+  get 'cocktails/update'
+  get 'cocktails/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
